a84eacf1bf82da42ee49d29227db8ecb4fe044c8
CodeKnight314 <rtang24@nmhschool.org>
2024-04-22 19:52:00 -0400

    Modified relevant files

--- loss.py
+++ loss.py
@@ -2,14 +2,16 @@ import torch
 import torch.nn as nn 
 
 class CrossEntropyLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, reduction = "mean"):
         super().__init__() 
+        self.reduction = reduction
     
     def forward(self, logits : torch.Tensor, labels : torch.Tensor):
         assert logits.shape == labels.shape and logits.size(1) != 1, "[ERROR] Logits and labels have incompatiable shapes or logits are shaped for binary classification."
         probs = torch.softmax(logits, dim=-1)
         log_probs = torch.log(probs + 1e-9)
-        return - (labels * log_probs).sum(dim=-1).mean().item()
+        loss = - (labels * log_probs)
+        return loss.sum(dim=-1).mean().item() if self.reduction == "mean" else loss.sum().item()
 
 class BCELoss(nn.Module): 
     def __init__(self): 
@@ -31,6 +33,7 @@ class HingeLoss(nn.Module):
         probs = torch.tanh(logits)
         loss = (1 - labels * probs).sum(dim=-1).mean().item()
         return max(0, loss)
+
 
 
 
