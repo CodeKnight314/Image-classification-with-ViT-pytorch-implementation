4f792c0f486f56707535ab8ce37182b107201cb7
CodeKnight314 <rtang24@nmhschool.org>
2024-04-22 20:17:54 -0400

    Modified relevant files

--- configs.py
+++ configs.py
@@ -3,6 +3,15 @@ import torch
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 root_dir = ""
+
 img_height = ""
 img_width = ""
 data_transform = ""
+
+model_save_path = ""
+
+batch_size = 32
+
+epochs = 20 
+
+output_dir = ""
