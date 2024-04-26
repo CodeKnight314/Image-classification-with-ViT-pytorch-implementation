90c6a4b6be9d570a7570a6a877dc45580dd0b9ba
CodeKnight314 <rtang24@nmhschool.org>
2024-04-26 13:50:55 -0400

    Modified relevant files

--- dataset.py
+++ dataset.py
@@ -43,8 +43,8 @@ class ImgClsDataset(Dataset):
     def __len__(self):
         return len(self.images)
     
-def load_dataset(batch_size = configs.batch_size, shuffle = True, mode = "train"): 
+def load_dataset(img_height = configs.img_height, img_width = configs.img_width, batch_size = configs.batch_size, shuffle = True, mode = "train"): 
     assert mode in ["train", "valid", "test"], "[ERROR] Invalid dataset mode"
-    ds = ImgClsDataset(configs.root_dir, configs.img_height, configs.img_wdith, mode = mode, transforms = configs.transforms)
+    ds = ImgClsDataset(configs.root_dir, img_height, img_width, mode = mode, transforms = configs.transforms)
     configs.num_class = len(ds.id_to_class_dict)
     return DataLoader(dataset=ds, batch_size=batch_size, shuffle=shuffle, drop_last=True)
