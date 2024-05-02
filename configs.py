4c3f48e436758f4302d8841ac7f9b99af8416b04
CodeKnight314 <rtang24@nmhschool.org>
2024-05-02 10:21:11 -0400

    Modified relevant files

--- configs.py
+++ configs.py
@@ -1,4 +1,5 @@
 import torch 
+import os
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -17,4 +18,25 @@ epochs = 35
 
 output_dir = "/workspace"
 
+log_output_dir = os.path.join(output_dir, "log_outputs")
+
+matrix_output_dir = os.path.join(output_dir, "conf_matrices")
+
 num_class = None
+
+id_to_category_dict = None
+
+category_to_id_dict = None
+
+def main(): 
+    if not os.path.exists(log_output_dir): 
+        os.makedirs(log_output_dir)
+        print("[INFO] Log output dir not found, Creating folder directory.")
+
+    if not os.path.exists(matrix_output_dir): 
+        os.makedirs(matrix_output_dir)
+        print("[INFO] Matrix output dir not found. Creating folder directory.")
+
+if __name__ == "__main__": 
+    main()
+    
